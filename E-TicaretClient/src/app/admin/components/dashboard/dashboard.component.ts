85b437458f483acff69c11e0c5e4140921029c85
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-04-28 18:14:13 +0700

    Client - Setup Alertify notification

--- E-TicaretClient/src/app/admin/components/dashboard/dashboard.component.ts
+++ E-TicaretClient/src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  constructor(private alertify: AlertifyService){}
+  ngOnInit(): void {
+   this.message();
+  }
+
+  message(){
+    this.alertify.message("Tran Hoang Duc", {
+      messageType: MessageType.Success,
+      delay: 5,
+      position: Position.BottomRight,
+
+    } )
+  }
+
+  d(){
+    this.alertify.dismiss();
+  }
 
 }
